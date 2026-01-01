4414fda4c4785b850d7be9fcb035e16c621e865d
qpdh <qpdh1924@gmail.com>
2026-01-01 17:04:11 +0900

    초기 커밋.

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using WPFBoilerPlate.ViewModels;
+
+namespace WPFBoilerPlate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public App()
+        {
+            ServiceProvider = ConfigureService();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private IServiceProvider ConfigureService()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<MainViewModel>();
+
+            services.AddTransient<MainWindow>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WPFBoilerPlate.ViewModels;
+
+namespace WPFBoilerPlate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel viewModel)
+        {
+            InitializeComponent();
+
+            this.DataContext = viewModel;
+        }
+    }
+}
